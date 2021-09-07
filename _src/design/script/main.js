1f0ac4e206f4716b3ac728485b2e611d3ea012f1
AlexKa <x182838@gmail.com>
2021-09-07 08:23:46 +0300

    all modals

--- _src/design/script/main.js
+++ _src/design/script/main.js
@@ -70,6 +70,28 @@ $('[data-selectric]').selectric();
   }
 })();
 
+(() => {
+  if(document.querySelector('.modal__list')) {
+    const modalItems = document.querySelectorAll('.modal__item');
+    const modalItemContents = document.querySelectorAll('.modal__item-content');
+
+    modalItems.forEach((elem, index) => {
+      elem.addEventListener('click', (evt) => {
+        modalItems.forEach((el) => {
+          el.classList.remove('_active');
+          evt.currentTarget.classList.add('_active');
+        });
+
+        modalItemContents.forEach((el, i, arr) => {
+          el.classList.remove('_active');
+          let ind = index;
+          arr[++ind].classList.add('_active');
+        });
+      })
+    });
+  }
+})();
+
 @@include('block/__scroll-to.js')
 @@include('block/__up.js')
 @@include('block/__stick-footer.js')
